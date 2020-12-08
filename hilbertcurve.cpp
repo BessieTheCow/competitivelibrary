cb7ca21894020f0e0009814046e7011ffff197a0
Alexander Zhang <zhangalexy@gmail.com>
2020-12-08 11:31:06 -0800

    Add parenthesis around bit operations

--- hilbertcurve.cpp
+++ hilbertcurve.cpp
@@ -17,7 +17,7 @@ long long hilbertorder(int x, int y)
 	for (int s = 1 << (logmaxn - 1); s; s >>= 1)
 	{
 		bool rx = x & s, ry = y & s;
-		d = d << 2 | rx * 3 ^ static_cast<int>(ry);
+		d = (d << 2) | ((rx * 3) ^ static_cast<int>(ry));
 		if (!ry)
 		{
 			if (rx)
